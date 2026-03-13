b8c090d49ac8a5db6d600fa6858ab8932f5f83c3
Omar R <omar.ramos19@gmail.com>
2026-03-13 12:55:56 -0700

    Basic filters layout

---
+++ src/streamlit_app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import geopandas as gpd
+
+st.set_page_config(
+    page_title="Vancouver Neighbourhood Safety Explorer", 
+    layout = "wide"
+)
+
+st.title("🍁 Vancouver Neighbourhood Safety")
+st.markdown(
+    "Explore where incidents cluster, which crime types are most common, "
+    "and review filtered records. For year 2025."
+)
+
+# Data
+@st.cache_data
+def load_crime_data():
+    return pd.read_csv("data/processed/processed_vancouver_crime_data_2025.csv")
+
+crime_df = load_crime_data()
+
+# Sidebar filters
+months_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+time_of_day_order = ["Morning", "Afternoon", "Evening/Night"]
+
+
+st.sidebar.header("Filters")
+
+selected_nb = st.sidebar.multiselect(
+    "Neighbourhood",
+    sorted(crime_df["NEIGHBOURHOOD"].unique()),
+    default=["Downtown", "West End"]
+)
+
+selected_crime_trype = st.sidebar.multiselect(
+    "Crime Type",
+    sorted(crime_df["TYPE"].unique()),
+    default=["Break and Enter Residential/Other"]
+)
+
+selected_month = st.sidebar.multiselect(
+    "Month",
+    months_order
+)
+
+selected_time_of_day = st.sidebar.multiselect(
+    "Time of Day",
+    time_of_day_order
+)
